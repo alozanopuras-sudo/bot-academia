9f8e64a6cdccdbbdc6916b911e542acc54b08c2a
alozanopuras-sudo <alozanopuras@gmail.com>
2026-05-03 12:13:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 import os
 from discord.ext import commands
 
-# Configuración de los permisos (Intents)
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -16,10 +15,10 @@ async def on_ready():
 async def hola(ctx):
     await ctx.send('¡Hola! Soy tu Tutor ALP y ya estoy vivo.')
 
-# IMPORTANTE: Asegúrate de que en Render la variable sea DISCORD_TOKEN
+# Esto lee la llave de la pestaña Ambiente de Render
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-if TOKEN is None:
-    print("❌ ERROR: No se encuentra la variable DISCORD_TOKEN en Render")
+if TOKEN:
+    bot.run(TOKEN)
 else:
-    bot.run('TU_TOKEN_REAL_AQUI_ENTRE_COMILLAS')
+    print("❌ ERROR: No se encontró la variable DISCORD_TOKEN")
